ced8cc135fda32babfb4f623c05486f3df470db8
iDimit <tsimintiad@mail.ru>
2022-09-22 00:26:17 +0400

    dz 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import './Style/App.css';
+import React, {useRef} from "react";
 import Message from "./Message";
 
-function App() {
-  const text = "Hi. I'm text in props"
+
+function App () {
+  const [messageList, setMessageList] = React.useState([]);
+
+  const [valueAuthor, setValueAuthor] = React.useState('');
+  const [valueText, setValueText] = React.useState('');
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    setMessageList(prevState => [...prevState, {
+      author: valueAuthor,
+      text: valueText,
+    }])
+    setValueAuthor('');
+    setValueText('');
+  }
+
   return (
-    <div className="App">
-        <Message text={text} />
-    </div>
+      <div className='container'>
+        <form className='form' onSubmit={handlerSubmit}>
+          <input type="text" value={valueAuthor} onChange={(e) => setValueAuthor(e.target.value)}/>
+          <input type="text" value={valueText} onChange={(e) => setValueText(e.target.value)}/>
+          <button className='button' type='submit'>Добавить</button>
+        </form>
+        {
+          messageList.map((message => {
+            return (
+                <div className="renderMessage">
+                 <h3 className="message">{message.text}</h3>  <p className="author">от  {message.author}</p>
+                </div>)
+          }))
+        }
+      </div>
+
+
   );
 }
 
-export default App;
+export default App
+;
